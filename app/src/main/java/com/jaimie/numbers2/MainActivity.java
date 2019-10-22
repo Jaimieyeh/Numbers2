a21de25b9c2b10e98a7219925dda32c44f797fc6
miia <miia@com.tw>
2019-10-22 15:37:42 +0800

    var

---
+++ app/src/main/java/com/jaimie/numbers2/MainActivity.java
@@ -0,0 +1,83 @@
+package com.jaimie.numbers2;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView number;
+    ImageView result;
+    int counter;
+    int secret = new Random().nextInt(10) + 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "secret" + secret);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        number = findViewById(R.id.num);
+        result = findViewById(R.id.emotion);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter ++;
+                number.setText(String.valueOf(counter));
+                result.setAlpha(1.0f);
+                result.setVisibility(View.VISIBLE);
+                if (counter == secret){
+                    Toast.makeText(MainActivity.this, "Oh noo!", Toast.LENGTH_LONG).show();
+                    result.setImageResource(R.drawable.anger);
+                } else {
+                    result.setImageResource(R.drawable.smile);
+                    result.animate().alpha(0.0f).setDuration(1200);
+                }
+            }
+        });
+    }
+    public void zero (View view){
+        counter = 0;
+        number.setText(String.valueOf(counter));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
+    }
+}
